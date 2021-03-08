e9fb86501f235920bbf77bfb08efe62e681ecbfa
theogvn <79159451+theogvn@users.noreply.github.com>
2021-03-08 18:10:42 +0100

    fixing issue blocking transport calculation Force creation of a GS directroy

--- exemple/run_transport.sh
+++ exemple/run_transport.sh
@@ -3,6 +3,7 @@ echo "starting transport"
 for i in 1 2 5 20 50
 do
   cd length$i
+  mkdir GS
   # setup the dftb_in.hsd for Transport
   cat dftb_in.hsd > dftb_in_contacts.hsd # duplicate dftb_in file
   transport_file_editor
