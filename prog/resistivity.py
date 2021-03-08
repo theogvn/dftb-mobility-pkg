0171b50c6348b8f6c80e158189d7698abebabf01
theogvn <79159451+theogvn@users.noreply.github.com>
2021-03-08 18:08:09 +0100

    graph always drawn for saving

--- prog/resistivity.py
+++ prog/resistivity.py
@@ -43,25 +43,29 @@ print('R value     : {:.4}\n'.format(r_value))
 print('Resistivity : {:.4e} ohm.m'.format(rho))
 print('conductivity: {:.4e} S/m'.format(sigma))
 
+plt.style.use('seaborn-whitegrid')
+plt.plot(L, R, '+', markeredgewidth=1.6, markersize=8)
+plt.xlabel('Length')
+plt.ylabel('Resistance (ohm)')
+
+# plotting linregress
+x1, y1 = min(L), min(L)*rho + regress[1]
+x2, y2 = max(L), max(L)*rho + regress[1]
+plt.axline((x1, y1), (x2, y2), ls='--', color='gray', lw=0.8,
+           label="linear regression")
+plt.legend()
+
 if r_value <= 0.90 and r_value > 0.70:
     print('\n\n')
     print('''/!\\ given the R value you might no be in the presence of a ohmic
           behaviour, hence the results might no be trustworthy /!\\''')
-    plt.style.use('seaborn-whitegrid')
-    plt.plot(L, R, '+', markeredgewidth=1.6, markersize=8)
-    plt.xlabel('Length')
-    plt.ylabel('Resistance (ohm)')
-
-    # plotting linregress
-    x1, y1 = min(L), min(L)*rho + regress[1]
-    x2, y2 = max(L), max(L)*rho + regress[1]
-    plt.axline((x1, y1), (x2, y2), ls='--', color='gray', lw=0.8,
-               label="linear regression")
-    plt.legend()
+    plt.show()
 
 if r_value <= 0.70:
     print('\n\n')
     print('''/!\\ given the R value you are not in the presence of a ohmic
     behaviour, hence the results are not trustworthy /!\\''')
     plt.show()
+
 plt.savefig('resistance_length-Graph.png')
+# ------------------------------- The End ------------------------------------ #
